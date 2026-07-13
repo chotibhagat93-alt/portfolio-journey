b83284db16ce646dadc0b81512ca40a38e470d43
Sakshi Bhagat <chotibhagat93@gmail.com>
2026-07-13 17:53:21 +0530

    Added data types and input programs

---
+++ Python/03_data_types.py
@@ -0,0 +1,14 @@
+"""
+Author: Sakshi Bhagat
+Topic: Data Types
+"""
+
+name = "Sakshi"
+age = 19
+cgpa = 7.2
+placed = False
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(placed))
