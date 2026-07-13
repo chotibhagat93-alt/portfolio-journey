0a0e3f2d2796ee81b8a861ace0a6be9007be71ef
Sakshi Bhagat <chotibhagat93@gmail.com>
2026-07-13 18:12:00 +0530

    Added data types, input and student profile programs

---
+++ Python/05_student_profile.py
@@ -0,0 +1,24 @@
+"""
+Author: Sakshi Bhagat
+Program: Student Profile
+"""
+
+name = input("Enter your name: ")
+div = input("Enter your division: ")
+branch = input("Enter your branch: ")
+year = input("Enter your year: ")
+cgpa = float(input("Enter your CGPA: "))
+
+print("\n===== Student Profile =====")
+print("Name   :", name)
+print("Division:", div)
+print("Branch :", branch)
+print("Year   :", year)
+print("CGPA   :", cgpa)
+
+print("\n===== Data Types =====")
+print(type(name))
+print(type(div))
+print(type(branch))
+print(type(year))
+print(type(cgpa))
