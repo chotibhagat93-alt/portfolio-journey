57c8de84bec0f8882c92225d9bfeeacd113bfff3
Sakshi Bhagat <chotibhagat93@gmail.com>
2026-07-25 08:33:09 +0530

    Added Python functions programs

---
+++ Python/17_addition_function.py
@@ -0,0 +1,14 @@
+"""
+Author: Sakshi Bhagat
+Topic: Addition Function
+"""
+
+def add(a, b):
+    return a + b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = add(num1, num2)
+
+print("Addition =", result)
