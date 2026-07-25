cd84054c0e85bc5a233013af7c12baae01cd17d0
Sakshi Bhagat <chotibhagat93@gmail.com>
2026-07-25 09:45:57 +0530

    Added Python lists programs

---
+++ Python/20_lists.py
@@ -0,0 +1,12 @@
+"""
+Author: Sakshi Bhagat
+Topic: Lists
+"""
+
+fruits = ["Apple", "Mango", "Banana", "Orange"]
+
+print(fruits)
+
+print("First Fruit :", fruits[0])
+print("Second Fruit:", fruits[1])
+print("Last Fruit  :", fruits[-1])
