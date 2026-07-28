aeb658feb6b35c8e60ddf1392234854728224f4c
Sakshi Bhagat <chotibhagat93@gmail.com>
2026-07-28 18:43:46 +0530

    Added favourite cars program

---
+++ Python/22_favorite_cars.py
@@ -0,0 +1,11 @@
+"""
+Author: Sakshi Bhagat
+Topic: Favorite Cars
+"""
+
+favorite_cars = ["Honda", "BMW", "SUV", "Jaguar"]
+
+print("===== My Favorite Cars =====")
+
+for i, car in enumerate(favorite_cars, start=1):
+    print(f"{i}. {car}")
