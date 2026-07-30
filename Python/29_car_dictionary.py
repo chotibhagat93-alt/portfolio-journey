b11522515f7b4722163d762c1721fa2588118855
Sakshi Bhagat <chotibhagat93@gmail.com>
2026-07-30 18:45:24 +0530

    Added dictionary programs

---
+++ Python/29_car_dictionary.py
@@ -0,0 +1,17 @@
+"""
+Author: Sakshi Bhagat
+Topic: Car Dictionary
+"""
+
+car = {
+    "brand": "BMW",
+    "model": "M5",
+    "color": "Black",
+    "price": "1.8 Crore"
+}
+
+print("----- Car Details -----")
+print("Brand :", car["brand"])
+print("Model :", car["model"])
+print("Color :", car["color"])
+print("Price :", car["price"])
