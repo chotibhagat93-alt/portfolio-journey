de66bced5fe086e1667a7a7705307c22a8df9632
Sakshi Bhagat <chotibhagat93@gmail.com>
2026-07-30 18:41:17 +0530

    Added dictionary programs

---
+++ Python/27_dictionary.py
@@ -0,0 +1,16 @@
+"""
+Author: Sakshi Bhagat
+Topic: Dictionary
+"""
+
+student = {
+    "name": "Sakshi",
+    "branch": "EXTC",
+    "year": "Third Year",
+    "cgpa": 7.3
+}
+
+print("Student Name :", student["name"])
+print("Branch       :", student["branch"])
+print("Year         :", student["year"])
+print("CGPA         :", student["cgpa"])
